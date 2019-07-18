a5caefb11ff8b67f3ba2b1831f4549b60420c5c0
DriverBen <2477822095@qq.com>
2019-07-18 12:16:02 +0800

    Site updated: 2019-07-18 12:15:59

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/about/index.html","c920236e6461def5bafe5f0e837193bf"],["/images/background.png","a57307218aecfdafb5c8153088d8f58c"],["/images/saber.png","2d678a46296f3a19c3d9e28132211d16"],["/index.html","76b68c9a1ed580a4cef501deb45b8062"],["/tags/index.html","2c54b07f3a9af99eb85f20c76d26c075"]];
+var precacheConfig = [["/about/index.html","d9f9b5d8edc428ebfa1a17a9c8ee76d8"],["/images/background.png","a57307218aecfdafb5c8153088d8f58c"],["/images/saber.png","2d678a46296f3a19c3d9e28132211d16"],["/index.html","dcfdfd96eaaf3e45c245bcf94a603aa4"],["/tags/index.html","acc8374fbdc3e61c46d27417ee547bc5"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
